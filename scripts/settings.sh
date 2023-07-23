959bd8f4a4a6aba8672c89fa3b7dd70d3efc49ee
motoi tanigaki <motoi@motoinoMacBook-Pro.local>
2023-07-23 17:54:35 +0900

    Modify settings.sh

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 origin_dir=~/.dotfiles/settings
+home_dir=$(eval echo ~)
+
+echo "Original directory: $origin_dir"
 
 find "$origin_dir" -type f | while read file
 do
-    target="${file/#$origin_dir/~}"
-    mkdir -p "$(dirname "$target")"
+    echo "Processing file: $file"
+    target="${file/#$origin_dir/$home_dir}"    
+    echo "Target link will be: $target"
+    target_dir=$(dirname "$target")
+    echo "Target directory is: $target_dir"
+    mkdir -p "$target_dir"
     ln -sf "$file" "$target"
+    echo "Symbolic link created"
 done
